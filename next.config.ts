4c535e3874f1c5e74283c1c6557240d31749f6e1
root <root@m3905.contaboserver.net>
2026-03-20 18:26:46 +0100

    chore: adiciona orquestração docker, nginx, ssl e bloqueio de headers

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.3.202'],
+  output: "standalone",
+  poweredByHeader: false,
 };
 
 export default nextConfig;
